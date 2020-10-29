57971c5f135c92187d04efa19b38dac9ade5f301
Ancia <anciakn40@gmail.com>
2020-10-29 12:55:51 +0530

    Created add user api call

---
+++ App/todoapp/lib/models/global.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = new Color(0xFF212128);
+Color redColor = new Color(0xFFE33E57);
+
+TextStyle intrayTitleStyle =new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: darkGreyColor,
+    fontSize: 30
+);
+TextStyle darkTodoTitle =new TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: darkGreyColor,
+    fontSize: 25
+);
